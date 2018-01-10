7a798f2b224741df0bddd10590a2f0b7a1081b46
Clayton Orman <COrman@jandlmarketing.com>
2018-01-10 14:18:29 -0500

    Initial commit

---
+++ IdentityServerQuickStart/Config.cs
@@ -0,0 +1,19 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerQuickStart
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "Clayton's API")
+            };
+        }
+    }
+}
