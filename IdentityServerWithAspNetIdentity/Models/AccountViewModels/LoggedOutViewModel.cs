ec8ea72b984fa0819a8e92ee9eeb53a37da1764e
Clayton Orman <COrman@jandlmarketing.com>
2018-01-15 16:40:06 -0500

    Add consent page ui

--- IdentityServerWithAspNetIdentity/Models/AccountViewModels/LoggedOutViewModel.cs
+++ IdentityServerWithAspNetIdentity/Models/AccountViewModels/LoggedOutViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace IdentityServerWithAspNetIdentity.Models.AccountViewModels
+﻿namespace IdentityServerWithAspNetIdentity.Models.AccountViewModels
 {
     public class LoggedOutViewModel
     {
